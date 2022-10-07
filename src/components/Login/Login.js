48c1d549302bfb146b02d6c071cacc52e427a39b
andriibildii <andrii.bildii@gmail.com>
2022-10-07 20:16:04 +0300

    login register app with react

---
+++ src/components/Login/Login.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export const Login = ({onFormSwitch}) => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('email: ' + email + ', ' + 'pass: ' + pass);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePass = (e) => {
+        setPass(e.target.value);
+    }
+
+
+    return (
+        <div className="form-container">
+            <h2>Login</h2>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="email">email</label>
+                <input 
+                    defaultValue={email} 
+                    onChange={handleEmail}
+                    type="email" 
+                    placeholder="email@gmail.com" 
+                    id="email" 
+                    name="email" 
+                />
+                <label htmlFor="password">password</label>
+                <input 
+                    defaultValue={pass} 
+                    onChange={handlePass} 
+                    type="password" 
+                    placeholder="********" 
+                    id="password" 
+                    name="password" 
+                    autoComplete="on"
+                />
+                <button type="submit">Log In</button>
+            </form>
+            <button className="link-btn" onClick={() => onFormSwitch('register')}>Don't have an account? Register here.</button>
+        </div>
+    )
+}
