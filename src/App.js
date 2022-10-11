1f7fade09f49669613f2c84c6ae626b748d07187
andriibildii <andrii.bildii@gmail.com>
2022-10-11 14:47:37 +0300

    new branch with use  react router dom to route  between login and register pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import {HomePage} from 'components/HomePage/HomePage';
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+import './App.css';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-    console.log(currentForm);
-  }
-
   return (
     <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
+      <Routes>
+      <Route exact path='/' element={<HomePage/>} />
+      <Route exact path='/login' element={<Login/>} />
+      <Route exact path='/register' element={<Register/>} />
+
+    </Routes>
     </div>
+    
   );
 }
 
